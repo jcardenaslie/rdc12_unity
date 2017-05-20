8ef7bbe1a96fe1c017d5e31ff897176ca9006b00
Joaquin Cardenas <jcardenas.lie@gmail.com>
2017-05-20 01:45:02 -0400

    Inicio repo

---
+++ Assets/RDC12/Script/opciones.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opciones : MonoBehaviour {
+    private int count=0;
+    private GameObject btnNext;
+
+
+    private void DeactivateNextButton()
+    {
+        //Transform panelMenu = this.transform.FindChild("BottomMenu");
+        //btnNext = panelMenu.transform.FindChild("Next").gameObject;
+        //btnNext.SetActive(false);
+    }
+
+    public void CheckGameState()
+    {
+        if (count==3)
+        {
+            Transform panelMenu = this.transform.FindChild("BottomMenu");
+            btnNext = panelMenu.transform.FindChild("Next").gameObject;
+            btnNext.SetActive(true);
+            Transform correcto = this.transform.FindChild("Correcto");
+            correcto.gameObject.SetActive(true);
+            Debug.Log(count);
+        }
+        else
+        {
+            Transform incorrecto = this.transform.FindChild("Incorrecto");
+            incorrecto.gameObject.SetActive(true);
+            Debug.Log(count);
+        }
+    }
+
+    public void Sumar()
+    {
+        count++;
+    }
+    public void Restar()
+    {
+        count--;
+    }
+    public void sum8()
+    {
+        if (count == 8)
+        {
+            Transform panelMenu = this.transform.FindChild("BottomMenu");
+            btnNext = panelMenu.transform.FindChild("Next").gameObject;
+            btnNext.SetActive(true);
+            Debug.Log(count);
+        }
+    }
+    public void inicializar()
+    {
+        count = 0;
+    }
+}
