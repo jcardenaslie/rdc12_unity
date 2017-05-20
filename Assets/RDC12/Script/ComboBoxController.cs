ff898d776615671f7d5085d23efabcf2e9952219
Joaquin Cardenas <jcardenas.lie@gmail.com>
2017-05-20 16:36:05 -0400

    creacion combobox Falta terminar

---
+++ Assets/RDC12/Script/ComboBoxController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComboBoxController : MonoBehaviour {
+
+    public bool showMenu;
+    public Text btnComboBox;
+    private string seleccion;
+    // Use this for initialization
+
+    public void ButtonShowMenu() {
+        if (!showMenu) {
+            showMenu = true;
+        } else {
+            showMenu = false;
+        }
+        Debug.Log("Set showMenu:" + showMenu);
+    }
+
+    private void SetComboBoxText(string s){
+        //btnComboBox.GetComponent<TextMesh>().text = s;
+        showMenu = false;
+    }
+    public void LaTirana() {
+        seleccion = "La Tirana";
+        SetComboBoxText(seleccion);
+    }
+    public void ElCuranto()
+    {
+        seleccion = "El Curanto";
+        SetComboBoxText(seleccion);
+    }
+    public void LasLeyendas()
+    {
+        seleccion = "Las Leyendas";
+        SetComboBoxText(seleccion);
+    }
+    public void FiestaTapi()
+    {
+        seleccion = "Fiesta Tapi";
+        SetComboBoxText(seleccion);
+    }
+    public void LosJuegos()
+    {
+        seleccion = "Los Juegos";
+        SetComboBoxText(seleccion);
+    }
+    public void LaMinga()
+    {
+        seleccion = "La Minga";
+        SetComboBoxText(seleccion);
+    }
+    public void LaDibam()
+    {
+        seleccion = "La Dibam";
+        SetComboBoxText(seleccion);
+    }
+
+}
---
+++ Assets/RDC12/Script/ComboBoxElementDropDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboBoxElementDropDown : MonoBehaviour {
+
+    private Animator buttonAnim;
+    public ComboBoxController btnCon;
+
+	// Use this for initialization
+	void Start () {
+        buttonAnim = GetComponent<Animator>();
+        //btnCon = GameObject.Find("opciones").GetComponent<ComboBoxController>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (btnCon.showMenu)
+        {
+            buttonAnim.SetBool("button_show", true);
+        }
+        else
+        {
+            buttonAnim.SetBool("button_show", false);
+        }
+    }
+}
